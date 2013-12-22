e826e397ca84c4e06ce952f79767291295992148
srobbibaro <steverobbibaro@gmail.com>
2013-12-22 01:25:12 -0500

    Cosmetic changes -  tab -> spaces

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -3,25 +3,25 @@ using System.Collections;
 
 public class Coin : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {}
-	
-	// Update is called once per frame
-	void Update () {}
-	
-	void OnCollisionEnter(Collision collision) {
-		if (collision.gameObject == GameObject.Find("MarblePlayer")) {
-			GameObject gameLevel = GameObject.Find("GameLevel");
-			
-			if (gameLevel != null) {
-        		GameLevel gameLevelScript = gameLevel.GetComponent<GameLevel>();
-        		gameLevelScript.incScore(1000);
-        		Destroy(gameObject);
-        		gameLevelScript.addNewCoin();
-			}
-			else {
-				Debug.Log("GameLevel was null in Coin:OnCollisionEnter");
-			}
-		}
+    // Use this for initialization
+    void Start () {}
+    
+    // Update is called once per frame
+    void Update () {}
+    
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject == GameObject.Find("MarblePlayer")) {
+            GameObject gameLevel = GameObject.Find("GameLevel");
+            
+            if (gameLevel != null) {
+                GameLevel gameLevelScript = gameLevel.GetComponent<GameLevel>();
+                gameLevelScript.incScore(1000);
+                Destroy(gameObject);
+                gameLevelScript.addNewCoin();
+            }
+            else {
+                Debug.Log("GameLevel was null in Coin:OnCollisionEnter");
+            }
+        }
     }
 }
